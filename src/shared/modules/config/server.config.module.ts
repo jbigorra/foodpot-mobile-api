72a0d293996ef2e296a993f91fc271309640424b
Juan bigorra <juanchoibf@gmail.com>
2021-09-25 15:01:39 +0200

    map to desire .env file according to NODE_ENV value

--- src/shared/modules/config/server.config.module.ts
+++ src/shared/modules/config/server.config.module.ts
@@ -5,11 +5,18 @@ import { serverConfigValidationSchema } from "./server-config.validation-schema"
 import { SupabaseConfig } from "./supabase.config";
 import { ServerConfig } from "./server.config";
 
+const envFileMap = {
+  local: ".env/.env.local",
+  dev: ".env/.env.dev",
+  prod: ".env/.env.prod"
+};
+
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [configuration],
-      validationSchema: serverConfigValidationSchema
+      validationSchema: serverConfigValidationSchema,
+      envFilePath: envFileMap[process.env.NODE_ENV] || envFileMap.local
     })
   ],
   providers: [ConfigService, SupabaseConfig, ServerConfig],
