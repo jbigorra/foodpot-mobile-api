791f05827bbd76178041010f55f65092c39e17bb
Juan bigorra <juanchoibf@gmail.com>
2021-09-26 21:54:26 +0200

    Change env variable name to PORT fopr heroku

--- src/shared/modules/config/main.config.ts
+++ src/shared/modules/config/main.config.ts
@@ -2,7 +2,7 @@
 export default () => ({
   env: process.env.NODE_ENV,
   server: {
-    port: process.env.SERVER_PORT || 3000
+    port: process.env.PORT || 3000
   },
   supabase: {
     apiUrl: process.env.SUPABASE_URL,
