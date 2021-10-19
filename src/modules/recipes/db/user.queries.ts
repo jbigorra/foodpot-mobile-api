242a1481f11852398dca75d6c4c2824302dd5a6b
Juan bigorra <juanchoibf@gmail.com>
2021-10-20 01:50:28 +0200

    Add RecipeQueries for main query by text

---
+++ src/modules/recipes/db/user.queries.ts
@@ -0,0 +1,38 @@
+import { Injectable } from "@nestjs/common";
+import { Recipe, Prisma } from "@prisma/client";
+import { DbClient } from "src/shared/modules/database/db.client";
+
+const recipeWithIngredients = Prisma.validator<Prisma.RecipeArgs>()({
+  include: { ingredients: true }
+});
+
+export type RecipeWithIngredients = Prisma.RecipeGetPayload<
+  typeof recipeWithIngredients
+>;
+
+@Injectable()
+export class RecipesQueries {
+  constructor(private db: DbClient) {}
+
+  async getManyByText(params: {
+    cursor?: number;
+    take?: number;
+    searchText?: string;
+    orderBy?: Prisma.RecipeOrderByWithRelationInput;
+  }): Promise<RecipeWithIngredients[]> {
+    const { cursor, take, searchText, orderBy } = params;
+    return this.db.recipe.findMany({
+      cursor: cursor ? { id: cursor } : undefined,
+      take,
+      orderBy,
+      where: {
+        searchTerms: {
+          contains: searchText
+        }
+      },
+      include: {
+        ingredients: true
+      }
+    });
+  }
+}
