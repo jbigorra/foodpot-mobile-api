be8f720733fc8b21dcec7c23b15e219184b2f7b4
Juan Bigorra <juanchoibf@gmail.com>
2021-11-06 21:13:16 +0100

    Extend Request type to add ContextUser to the request object

---
+++ src/types/express.types.d.ts
@@ -0,0 +1,16 @@
+export interface IContextUser {
+  readonly uuid: string;
+  readonly email: string;
+  readonly phone: string;
+  readonly fullName?: string;
+  readonly provider?: string;
+}
+
+declare global {
+  declare namespace Express {
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    export interface Request {
+      user?: IContextUser;
+    }
+  }
+}
