30beea787fd9683dfc4bf7e3a1ad4aedebbfc9df
Juan Bigorra <juanchoibf@gmail.com>
2021-11-06 21:21:06 +0100

    Apply middleware to all routes, remove unnecesary controller and service from app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,15 @@
-import { Module } from "@nestjs/common";
+import { MiddlewareConsumer, Module } from "@nestjs/common";
 import { ServerConfigModule } from "./shared/modules/config/server.config.module";
 import { RecipesModule } from "./modules/recipes/recipes.module";
 import { DbModule } from "./shared/modules/database/db.module";
-import { AuthController } from "./modules/auth/auth.controller";
 import { AuthModule } from "./modules/auth/auth.module";
-import { AuthService } from "./modules/auth/auth.service";
+import { AuthMiddleware } from "./middleware";
 
 @Module({
-  imports: [ServerConfigModule, RecipesModule, DbModule, AuthModule],
-  controllers: [AuthController],
-  providers: [AuthService]
+  imports: [ServerConfigModule, RecipesModule, DbModule, AuthModule]
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer): void {
+    consumer.apply(AuthMiddleware).forRoutes("*");
+  }
+}
