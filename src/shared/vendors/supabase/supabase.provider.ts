526d96ce01cd06029df99a60005406c2750371f9
Juan bigorra <juanchoibf@gmail.com>
2021-11-14 01:09:52 +0100

    Add script to request password change from template

--- src/shared/vendors/supabase/supabase.provider.ts
+++ src/shared/vendors/supabase/supabase.provider.ts
@@ -1,6 +1,13 @@
 import { SupabaseConfig } from "../../modules/config/supabase.config";
 import { createClient, SupabaseClient } from "@supabase/supabase-js";
 
+export enum SupabaseAuthType {
+  SIGNUP = "signup",
+  MAGICLINK = "magiclink",
+  RECOVERY = "recovery",
+  INVITE = "invite"
+}
+
 export const SUPABASE_CLIENT = "SUPABASE_CLIENT";
 export const supabaseProvider = {
   provide: SUPABASE_CLIENT,
